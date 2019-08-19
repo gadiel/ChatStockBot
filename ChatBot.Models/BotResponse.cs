cb076b3d0ced641504ba0416483579a37b42ea30
Gadiel Ortez <ortez@outlook.com>
2019-08-19 14:56:49 -0500

    Created ChatBot.Models project to host the BotResponse class that will be used by ChatLogicLayer and ChatBotBroker

---
+++ ChatBot.Models/BotResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ChatBot.Models
+{
+    public class BotResponse
+    {
+        public string BotName { get; set; }
+        public string Message { get; set; }
+    }
+}
